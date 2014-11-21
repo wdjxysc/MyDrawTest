bd15cb7c54aa259a92f8aa1120490b3ebb54823d
LostTemple <wdjxysc@163.com>
2014-11-21 15:04:48 +0800

    Initial Commit

---
+++ MyDrawTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyDrawTest
+//
+//  Created by djwang on 14/11/21.
+//  Copyright (c) 2014年 djwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyDrawTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyDrawTest
+//
+//  Created by djwang on 14/11/21.
+//  Copyright (c) 2014年 djwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
